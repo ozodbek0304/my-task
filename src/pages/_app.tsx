efe366c48aaf39e62ea22b070e7c097217bbcf08
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2025-01-05 16:58:34 +0500

    sss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from '../components/ui/sonner';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ThemeProvider } from 'next-themes';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const queryClient = new QueryClient();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider attribute="class">
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <Toaster />
+        {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
 }
+
+export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="uz">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Your default app description here" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
